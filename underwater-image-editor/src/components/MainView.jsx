0a20d643c39bb5625ff9f508138323ff56cd9bbc
Aleksandr Kovalev <alex.kovalev010@gmail.com>
2024-07-11 12:58:05 -0400

    added pagination on settings cards

--- underwater-image-editor/src/components/MainView.jsx
+++ underwater-image-editor/src/components/MainView.jsx
@@ -6,42 +6,71 @@ import UpLoadButton from './UpLoadButton.jsx'
 import SettingsCard from './SettingsCard.jsx'
 import ImagePreview from './ImagePreview.jsx'
 
-
-
 const MainView = () => {
+  const [imgFile, setImgFile] = useState(null);
+  const [page, setPage] = useState(0);
+  const [brightnessValue, setBrightnessValue] = useState(0);
+  const [contrastValue, setContrastValue] = useState(0);
+  const [redLevel, setRedLevel] = useState(0);
+  const [hueLevel, setHueLevel] = useState(0);
+  const [saturationLevel, setSaturationLevel] = useState(0);
 
-    const [imgFile, setImgFile] = useState(null);
-    useEffect(() => {
-        let openImgOnLoad = false;
+  useEffect(() => {
+    let openImgOnLoad = false;
 
-        if (!openImgOnLoad) 
-            setImgFile(sampleImg);
-    }, []);
+    if (!openImgOnLoad) 
+        setImgFile(sampleImg);
+  }, []);
 
-    //console.log(brightnessValue);
-    //console.log(contrastValue);
-    //console.log(redLevel);
-    //console.log(hueLevel);
-    //console.log(saturationLevel);
+  const settingsPages = [
+    {
+      settings: [
+        { label: 'Brightness', min: -100, max: 100, value: brightnessValue, step: 0.5 },
+        { label: 'Contrast', min: -100, max: 100, value: contrastValue, step: 0.5 }
+      ],
+      setters: [setBrightnessValue, setContrastValue]
+    },
+    {
+      settings: [
+        { label: 'Red Level', min: -100, max: 100, value: redLevel, step: 0.5 }
+      ],
+      setters: [setRedLevel]
+    },
+    {
+      settings: [
+        { label: 'Hue', min: -100, max: 100, value: hueLevel, step: 0.5 },
+        { label: 'Saturation', min: -100, max: 100, value: saturationLevel, step: 0.5 }
+      ],
+      setters: [setHueLevel, setSaturationLevel]
+    }
+  ];
 
-    //ToDO
-    // the image sliders should be in cards that are paged left and right.
+  const handleNext = () => {
+    setPage((prevPage) => (prevPage + 1) % settingsPages.length);
+  };
+
+  const handlePrev = () => {
+    setPage((prevPage) => (prevPage - 1 + settingsPages.length) % settingsPages.length);
+  };
 
   return (
     <div className={styles.container}>
-        <div className={styles.imgContainer}>
-            {imgFile && <ImagePreview src={imgFile} />}
-            {/*<img src={imgFile} alt="Image Area"/>*/}
-        </div>
-        <SettingsCard />
-        
-        <div className={styles.imgControls}>
-            <UpLoadButton  setImgFile={setImgFile} /> 
-            <button>Reset</button>
-            <button>Download</button>
-        </div>
-    </div>  
-  )
-}
+      <div className={styles.imgContainer}>
+        {imgFile && <ImagePreview src={imgFile} />}
+      </div>
+      <SettingsCard 
+        settings={settingsPages[page].settings} 
+        setters={settingsPages[page].setters} 
+        handleNext={handleNext} 
+        handlePrev={handlePrev} 
+      />
+      <div className={styles.imgControls}>
+        <UpLoadButton setImgFile={setImgFile} />
+        <button>Reset</button>
+        <button>Download</button>
+      </div>
+    </div>
+  );
+};
 
-export default MainView
+export default MainView;
--- underwater-image-editor/src/components/SettingsCard.jsx
+++ underwater-image-editor/src/components/SettingsCard.jsx
@@ -1,51 +1,32 @@
-import PropTypes from 'prop-types'
-import React, { Component, useState } from 'react'
-import styles from './SettingsCard.module.css'
-import RangeSlider from './RangeSlider.jsx'
+import React from 'react';
+import RangeSlider from './RangeSlider.jsx';
+import styles from './SettingsCard.module.css';
 
-const SettingsCard = () => {
-
-    const [brightnessValue, setBrightnessValue] = useState(0);
-    const [contrastValue, setContrastValue ] = useState(0);
-    const [redLevel, setRedLevel ] = useState(0);
-    const [hueLevel, setHueLevel ] = useState(0);
-    const [saturationLevel, setSaturationLevel ] = useState(0);
-
-    console.log(brightnessValue);
-    console.log(contrastValue);
-    console.log(redLevel);
-    console.log(hueLevel);
-    console.log(saturationLevel);
-
-    return (
-      <div className={styles.container}>
-        <small>SettingsCard</small>
-        <div className={styles.sliders}>
-            <small>Brightness</small>
-            <div className={styles.slider}>
-                <RangeSlider min={-100} max={100} value={0} step={0.5} setValue={setBrightnessValue}/>
-            </div>
-            <small>Contrast</small>
-            <div className={styles.slider}>
-                <RangeSlider min={-100} max={100} value={0} step={0.5} setValue={setContrastValue}/>
-            </div>
-        </div>
-        
-         {/*
-        <small>Red Level</small>
-        <div>
-            <RangeSlider min={-100} max={100} value={0} step={0.5} setValue={setRedLevel}/>
-        </div>
-        <small>Hue</small>
-        <div>
-            <RangeSlider min={-100} max={100} value={0} step={0.5} setValue={setHueLevel}/>
-        </div>
-        <small>Saturation</small>
-        <div>
-            <RangeSlider min={-100} max={100} value={0} step={0.5} setValue={setSaturationLevel}/>
-        </div>*/}
+const SettingsCard = ({ settings, setters, handleNext, handlePrev }) => {
+  return (
+    <div className={styles.container}>
+      <small>SettingsCard</small>
+      <div className={styles.sliders}>
+        {settings.map((setting, index) => (
+          <div key={index} className={styles.slider}>
+            <small>{setting.label}</small>
+            <RangeSlider
+              min={setting.min}
+              max={setting.max}
+              value={setting.value}
+              step={setting.step}
+              setValue={setters[index]}
+            />
+          </div>
+        ))}
       </div>
-    )
-}
+      <div className={styles.paginationControls}>
+        <button className={`${styles.left}`} onClick={handlePrev}>&lt;</button>
+        <button className={`${styles.right}`} onClick={handleNext}>&gt;</button>
+      </div>
+    </div>
+  );
+};
 
-export default SettingsCard
+
+export default SettingsCard;
